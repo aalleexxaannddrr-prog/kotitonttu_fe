2eefae61819f4a07c5c55cf433cd5bb27e9e651e
benchik <olgalitvinova1905@icloud.com>
2024-07-31 01:07:32 +0200

    updt dataSlice, servicesSlice, serviceCreationSlice, typesSlice

--- src/components/TypesBrand/TypesContainer/TypesContainer.jsx
+++ src/components/TypesBrand/TypesContainer/TypesContainer.jsx
@@ -6,6 +6,12 @@ import TypeCard from "../TypeCard/TypeCard";
 export default function TypesContainer() {
 	const { types } = useSelector(state => state.types);
 
+	// Добавляем проверку типа данных
+	if (!Array.isArray(types)) {
+		console.error("Expected types to be an array but got:", types);
+		return <div>Error: Data is not in expected format.</div>;
+	}
+
 	return (
 		<div className={styles.wrapper}>
 			<div className='container'>
